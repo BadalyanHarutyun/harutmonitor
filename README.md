31fad8677067e4b17c7c2bc8bf2f26d226721510
Harut <badalyanharutyun@gmail.com>
2025-05-03 12:47:20 +0400

    windows binary and logs monitor optional flag to write monitor logs to file

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ### Option 1: Using `make` (recommended)
 Make sure you have `make` and `wget` installed. Then simply run:
-
+you can move you binary where you want /usr/local/bin/ or /usr/bin/ or similar place
 ```bash
 make install
 ```
@@ -12,6 +12,11 @@ wget -O harutmonitor https://raw.githubusercontent.com/BadalyanHarutyun/harutmon
 chmod +x harutmonitor
 sudo mv harutmonitor /usr/local/bin/
 ```
+
+## Windows dowload and installation
+curl -LO https://raw.githubusercontent.com/BadalyanHarutyun/harutmonitor/main/binaries/windows/harutmonitor.exe
+move harutmonitor.exe C:\Windows\System32\
+
 ## Usage
 
 After installing `harutmonitor`, you can use it like any other command-line tool.
@@ -20,3 +25,4 @@ After installing `harutmonitor`, you can use it like any other command-line tool
 
 ```bash
 harutmonitor sleep 5 && echo Harut
+harutmonitor --monitor-log=mylogs.log sleep 5 && echo Harut
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -8,72 +9,116 @@ import (
 	"time"
 
 	"github.com/fatih/color"
+	"github.com/shirou/gopsutil/v3/process"
 )
 
 func main() {
-	// Check if there are enough arguments
-	if len(os.Args) < 2 {
-		color.Red("Usage: go run command.go <command> [args...]")
+	var monitorLog string
+	flag.StringVar(&monitorLog, "monitor-log", "", "Log file to write process monitoring data")
+	flag.Parse()
+
+	// Get command and args
+	args := flag.Args()
+	if len(args) < 1 {
+		color.Red("Usage: harutmonitor [--monitor-log=file] <command> [args...]")
 		return
 	}
 
-	// Get the command and its arguments
-	command := os.Args[1]
-	args := os.Args[2:]
+	command := args[0]
+	cmdArgs := args[1:]
 
-	// Build the command
+	// Start the command
 	start := time.Now()
-	cmd := exec.Command(command, args...)
-
-	// Redirect the output to the Go program's stdout and stderr
+	cmd := exec.Command(command, cmdArgs...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	// Start the command
-	color.Cyan("Executing command: %s %s", command, strings.Join(args, " "))
+	color.Cyan("Executing command: %s %s", command, strings.Join(cmdArgs, " "))
+
 	err := cmd.Start()
 	if err != nil {
 		color.Red("Error starting command: %v", err)
 		return
 	}
 
-	// Display process details
-	displayProcessDetails(cmd)
+	proc, err := process.NewProcess(int32(cmd.Process.Pid))
+	if err != nil {
+		color.Red("Failed to get process info: %v", err)
+		return
+	}
 
-	// Start a goroutine to monitor process details
+	displayProcessDetails(proc)
+
+	// Monitor in a separate goroutine
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
-	go monitorProcess(cmd, ticker)
+	done := make(chan struct{})
+	if monitorLog != "" {
+		go monitorToFile(proc, ticker, done, monitorLog)
+	} else {
+		go monitorToConsole(proc, ticker, done)
+	}
 
-	// Wait for the command to finish
 	err = cmd.Wait()
+	close(done) // stop the monitoring goroutine
+
 	if err != nil {
 		color.Red("Command terminated with error: %v", err)
 	} else {
 		color.Green("Command executed successfully in %d ms", time.Since(start).Milliseconds())
 	}
+
+	// Final display of process details
+	displayProcessDetails(proc)
 }
 
-// Display process details for the given command
-func displayProcessDetails(cmd *exec.Cmd) {
-	psCmd := exec.Command("ps", "-p", fmt.Sprintf("%d", cmd.Process.Pid), "-o", "pid,ppid,cmd,%mem,%cpu,etime")
-	psOutput, err := psCmd.CombinedOutput()
-	if err != nil {
-		color.Red("Error getting process details: %v", err)
-		return
-	}
-	color.Yellow("Process details for PID %d:\n%s", cmd.Process.Pid, string(psOutput))
+// Display process details (one-time)
+func displayProcessDetails(p *process.Process) {
+	memPercent, _ := p.MemoryPercent()
+	cpuPercent, _ := p.CPUPercent()
+	cmdline, _ := p.Cmdline()
+	createTime, _ := p.CreateTime()
+	elapsed := time.Since(time.UnixMilli(createTime))
+
+	color.Yellow("Process Details:\nPID: %d\nCMD: %s\nCPU: %.2f%%\nMemory: %.2f%%\nElapsed: %s\n",
+		p.Pid, cmdline, cpuPercent, memPercent, elapsed.Truncate(time.Second))
 }
 
-// Monitor the process periodically and display details
-func monitorProcess(cmd *exec.Cmd, ticker *time.Ticker) {
-	for range ticker.C {
-		psCmd := exec.Command("ps", "-p", fmt.Sprintf("%d", cmd.Process.Pid), "-o", "pid,ppid,cmd,%mem,%cpu,etime")
-		psOutput, err := psCmd.CombinedOutput()
-		if err != nil {
-			color.Red("Error getting process details: %v", err)
+// Monitor process and print to console
+func monitorToConsole(p *process.Process, ticker *time.Ticker, done <-chan struct{}) {
+	for {
+		select {
+		case <-ticker.C:
+			displayProcessDetails(p)
+		case <-done:
 			return
 		}
-		color.Yellow("Process details for PID %d:\n%s", cmd.Process.Pid, string(psOutput))
+	}
+}
+
+// Monitor process and write to a log file
+func monitorToFile(p *process.Process, ticker *time.Ticker, done <-chan struct{}, logFile string) {
+	file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		color.Red("Failed to open log file: %v", err)
+		return
+	}
+	defer file.Close()
+
+	for {
+		select {
+		case <-ticker.C:
+			memPercent, _ := p.MemoryPercent()
+			cpuPercent, _ := p.CPUPercent()
+			cmdline, _ := p.Cmdline()
+			createTime, _ := p.CreateTime()
+			elapsed := time.Since(time.UnixMilli(createTime))
+
+			logLine := fmt.Sprintf("%s | PID: %d | CMD: %s | CPU: %.2f%% | Mem: %.2f%% | Elapsed: %s\n",
+				time.Now().Format(time.RFC3339), p.Pid, cmdline, cpuPercent, memPercent, elapsed.Truncate(time.Second))
+			file.WriteString(logLine)
+		case <-done:
+			return
+		}
 	}
 }
